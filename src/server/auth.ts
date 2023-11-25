fa5c5810f4c4ed9c1268526a6e1b94052b385585
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2023-11-25 19:35:14 +0500

    test api added

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -12,12 +12,7 @@ import { api } from "@/trpc/server";
 import { compare } from "bcrypt";
 import { randomUUID } from "crypto";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
+
 declare module "next-auth" {
   interface Session extends DefaultSession, User {
     user: {
@@ -27,51 +22,43 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-
   interface User {
-    // ...other properties
     paypal_token: string
     role: string;
   }
 
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session({ session, user, token }) {
-
-      const { email, sub, paypal_token, role, iat, exp, jti } = token
+      const { email, paypal_token, role } = token;
       return {
         ...session,
         user: {
           ...session.user,
           email: email,
           role: role,
-          paypal_token: paypal_token
-        }
-      }
+          paypal_token: paypal_token,
+        },
+      };
     },
     jwt({ token, user }) {
       if (user) {
-        token.paypal_token = user.paypal_token
-        token.email = user.email
-        token.role = user.role
-        return token
+        token.paypal_token = user.paypal_token;
+        token.email = user.email;
+        token.role = user.role;
+        return token;
       }
-      return token
+      return token;
     },
   },
   adapter: PrismaAdapter(db),
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   pages: {
-    signIn: '/'
+    signIn: "/",
   },
   secret: "POKOq/ooMmBaUcsKQfOeWkhXzwo5CTQ/vTiCARhS1vQ=",
   providers: [
@@ -79,53 +66,52 @@ export const authOptions: NextAuthOptions = {
       name: "Credentials",
       credentials: {
         email: { label: "email", type: "email", placeholder: "Abdul Rehman" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        
-        if (!credentials?.email || !credentials?.password)
-          return null
-        
-        const userInfo = await api.register.sellerInfo.query({userName:credentials.email})
-        if (!userInfo)
-          return null
-        
-        const result: boolean = await compare(credentials.password, userInfo.password)
-        if (!result)
-          return null
+        try {
+          if (!credentials?.email || !credentials?.password) return null;
 
-        const paypal_token = await api.paypal.getAuthToken.query()
-        if (!paypal_token)
-          return null
-        
-        return {
-          id: randomUUID().toString(),
-          email: credentials?.email,
-          role: "Seller",
-          paypal_token: paypal_token
+          const userInfo :{id:number,userName:string,password:string} | null = await api.register.sellerInfo.query({
+            userName: credentials.email,
+          });
+
+          if (!userInfo) {
+            console.error("User info not found");
+            return null;
+          }
+
+          const result: boolean = await compare(
+            credentials.password,
+            userInfo.password
+          );
+
+          if (!result) {
+            console.error("Password comparison failed");
+            return null;
+          }
+
+          const paypal_token = await api.paypal.getAuthToken.query();
+
+          if (!paypal_token) {
+            console.error("Failed to get PayPal token");
+            return null;
+          }
+
+          return {
+            id: randomUUID().toString(),
+            email: credentials?.email,
+            role: "Seller",
+            paypal_token: paypal_token,
+          };
+        } catch (error) {
+          console.error("Error in authorize function:", error);
+          return null;
         }
-      }
-    })
-    // DiscordProvider({
-    //   clientId: env.DISCORD_CLIENT_ID,
-    //   clientSecret: env.DISCORD_CLIENT_SECRET,
-    // }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+      },
+    }),
+    // ... (other providers)
   ],
 };
 
-
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
 export const getServerAuthSession = () => getServerSession(authOptions);
