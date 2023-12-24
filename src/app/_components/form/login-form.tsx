f9c88243fd0290454670992cdfb2d9371c8e8ac1
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2023-12-24 07:22:43 +0500

    sort started

--- src/app/_components/form/login-form.tsx
+++ src/app/_components/form/login-form.tsx
@@ -5,12 +5,15 @@ import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { signIn } from 'next-auth/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react"
+import { divide } from "lodash"
 
 
 export const LoginForm = () => {
 
     const [visible, setVisible] = useState<boolean>(false)
     const [submiting, setSubmiting] = useState<boolean>(false)
+    const [alert, setAlert] = useState<boolean>(false)
     const router = useRouter()
 
     const { register, formState: { errors }, handleSubmit } = useForm()
@@ -24,45 +27,41 @@ export const LoginForm = () => {
             redirect: false,
         })
         setSubmiting((prev) => !prev)
+        console.log(signInData?.error)
         if (signInData?.error)
-            console.log('s')
+            setAlert(() => true)
         if (!signInData?.error)
             router.push("/dashboard")
     }
 
     return (
-        <form onSubmit={handleSubmit(formSubmitted)} className="grid grid-cols-2  gap-2">
-            <div className="flex flex-col gap-4">
-                <label className="text-gray-900  font-sans text-base md:text-xl">
-                    Quantity
+        <form onSubmit={handleSubmit(formSubmitted)} className="grid grid-cols-2  [&_input]:bg-transparent  [&_input]:p-2 gap-1">
+            {alert && <div className="col-span-2 text-red-600">
+                <Alert status='warning'>
+                    <AlertIcon />
+                    <AlertTitle>Email or Password is wrong!</AlertTitle>
+                </Alert>
+            </div>}
+            <div className="col-span-2 flex flex-col gap-2">
+                <label className="text-white  font-sans text-base md:text-xl">
+                    Email
                 </label>
-                <input {...register('quantity', {
-                    required: 'Quantity is required',
-                    min: {
-                        value: 1,
-                        message: 'Quantity must be greater than or equal to 1',
-                    },
-                    setValueAs: (value) => Math.max(1, parseInt(value) || 1),
+                <input {...register('email', {
+                    required: 'Email is required',
                 })}
-                    onChange={(e) => e.target.value = String(Math.max(1, parseInt(e.target.value)))}
-                    type="number" className="w-52 h-10 border-2 rounded-md p-2" defaultValue={1} />
-                <div className="text-red-600">{errors.quantity?.message?.toString()}</div>
+
+                    type="email" className="placeholder-shown:text-white placeholder:text-white border-1 rounded-md" placeholder="example@gmail.com" />
+                <div className="text-red-600">{errors.email?.message?.toString()}</div>
             </div>
-            <div className="flex flex-col gap-4">
-                <label className="text-gray-900  font-sans text-base md:text-xl">
-                    Quantity
+            <div className="col-span-2 flex flex-col gap-4 ">
+                <label className="text-white  font-sans text-base md:text-xl">
+                    Password
                 </label>
                 <div className="flex">
-                    <input {...register('quantity', {
-                        required: 'Quantity is required',
-                        min: {
-                            value: 1,
-                            message: 'Quantity must be greater than or equal to 1',
-                        },
-                        setValueAs: (value) => Math.max(1, parseInt(value) || 1),
+                    <input {...register('password', {
+                        required: 'Password is required',
                     })}
-                        onChange={(e) => e.target.value = String(Math.max(1, parseInt(e.target.value)))}
-                        type="number" className="w-52 h-10 border-2 rounded-md p-2" defaultValue={1} />
+                        type={visible ? "text" : "password"} className="placeholder-shown:text-white placeholder:text-white outline-0 border-1  border-r-0 rounded-md rounded-r-none w-full" placeholder="********" />
                     <button
                         type="button"
                         className="border-[1px] border-l-0 outline-0 rounded-md rounded-l-none px-1"
@@ -74,11 +73,11 @@ export const LoginForm = () => {
                     </button>
                 </div>
 
-                <div className="text-red-600">{errors.quantity?.message?.toString()}</div>
+                <div className="text-red-600">{errors.password?.message?.toString()}</div>
             </div>
-            <button type="submit" className="col-span-2 bg-yellow-600 text-white">
+            <button type="submit" className="col-span-2 bg-yellow-600 text-white py-2 rounded-md">
                 {submiting ? "Login..." : "Login"}
             </button>
-        </form>
+        </form >
     )
 }
